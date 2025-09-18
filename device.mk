a02374da45b20d0c656db9c1847b10dd5549577b
xt0032rus <andrey.denikin9@mail.com>
2025-09-18 23:33:25 +0200

    fuxi: add miui camera
    from https://codeberg.org/lolipuru/vendor_xiaomi_miuicamera

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/config/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
 
+# Camera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
 # eUICC
 PRODUCT_PACKAGES += \
     XiaomiEuicc
